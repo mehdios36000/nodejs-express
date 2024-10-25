c9559add3fe832d22f72136f432665ca0f674ec5
mehdios36000 <M.alamiidrissi@aui.ma>
2024-10-25 17:19:25 +0100

    first commit

--- dist/config/database.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const client_1 = require("@prisma/client");
-const prismaClientSingleton = () => {
-    return new client_1.PrismaClient();
-};
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
-exports.default = prisma;
-//# sourceMappingURL=database.js.map
